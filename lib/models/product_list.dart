a79a0e7398aec0cffcec385479fdc3e2e53f8d4c
souzap371 <souzap371@gmail.com>
2022-10-01 08:01:18 -0300

    persistencia ao banco de dados

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -7,10 +7,9 @@ import 'package:http/http.dart' as http;
 import 'package:shop/exceptions/http_exception.dart';
 
 import 'package:shop/models/product.dart';
+import 'package:shop/utils/contants.dart';
 
 class ProductList with ChangeNotifier {
-  final _baseUrl =
-      'https://shop-cod3r-80940-default-rtdb.firebaseio.com/products';
   final List<Product> _items = [];
 
   List<Product> get items => [..._items];
@@ -24,7 +23,7 @@ class ProductList with ChangeNotifier {
   Future<void> loadProducts() async {
     _items.clear();
     final response = await http.get(
-      Uri.parse('$_baseUrl.json'),
+      Uri.parse('${Constants.PRODUCT_BASE_URL}.json'),
     );
     if (response.body == 'null') return;
     Map<String, dynamic> data = jsonDecode(response.body);
@@ -63,7 +62,7 @@ class ProductList with ChangeNotifier {
 
   Future<void> addProduct(Product product) async {
     final response = await http.post(
-      Uri.parse('$_baseUrl.json'),
+      Uri.parse('${Constants.PRODUCT_BASE_URL}.json'),
       body: jsonEncode(
         {
           "name": product.name,
@@ -92,7 +91,7 @@ class ProductList with ChangeNotifier {
 
     if (index >= 0) {
       await http.patch(
-        Uri.parse('$_baseUrl/${product.id}.json'),
+        Uri.parse('${Constants.PRODUCT_BASE_URL}/${product.id}.json'),
         body: jsonEncode(
           {
             "name": product.name,
@@ -116,7 +115,7 @@ class ProductList with ChangeNotifier {
       notifyListeners();
 
       final response = await http.delete(
-        Uri.parse('$_baseUrl/${product.id}.json'),
+        Uri.parse('${Constants.PRODUCT_BASE_URL}/${product.id}.json'),
       );
       if (response.statusCode >= 400) {
         _items.insert(index, product);
---
+++ lib/utils/contants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const PRODUCT_BASE_URL =
+      'https://shop-cod3r-80940-default-rtdb.firebaseio.com/products';
+}
