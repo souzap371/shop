a2d38229f106ca6b6bab192d56bd42a21e36dfff
souzap371 <souzap371@gmail.com>
2022-10-11 20:49:34 -0300

    Aplicando animações entre telas e nos produtos

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:shop/pages/product_detail_page.dart';
 import 'package:shop/pages/product_form_page.dart';
 import 'package:shop/pages/products_page.dart';
 import 'package:shop/utils/app_routes.dart';
+import 'package:shop/utils/custom_route.dart';
 import 'pages/cart_page.dart';
 
 void main() {
@@ -58,6 +59,12 @@ class MyApp extends StatelessWidget {
             secondary: Colors.deepOrange,
           ),
           fontFamily: 'Lato',
+          pageTransitionsTheme: PageTransitionsTheme(
+            builders: {
+              TargetPlatform.iOS: CustomPageTransitionsBuilder(),
+              TargetPlatform.android: CustomPageTransitionsBuilder(),
+            },
+          ),
         ),
         // home: const ProductsOverviewPage(),
         routes: {
